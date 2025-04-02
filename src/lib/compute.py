08f60d872d6f02bd7c966cf4130b0619c58d89f7
JJNobis <jjnobis@gmail.com>
2025-04-02 08:36:31 -0700

    added Mult feature branch

---
+++ src/lib/compute.py
@@ -0,0 +1,16 @@
+class Compute:
+	def _init_(self, operator, operands):
+		self.operator = operator
+		self.operands = operands
+
+	def add(self):
+		pass
+
+	def subtract(self):
+		pass
+
+	def divide(self):
+		pass
+
+	def multiply(self):
+		pass
